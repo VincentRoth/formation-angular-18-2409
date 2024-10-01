73ba4060cec3b385c0f6ebe983117abd2e903d36
Vincent Roth <vincent@placeme.io>
2024-10-01 16:51:21 +0200

    Observable - Gestion de la désouscription

--- src/app/animal/animal-details/animal-details.component.ts
+++ src/app/animal/animal-details/animal-details.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Animal } from '../../shared/api/models/animal';
 import { AnimalService } from '../../shared/api/animal.service';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-animal-details',
   templateUrl: './animal-details.component.html',
   styleUrl: './animal-details.component.scss',
 })
-export class AnimalDetailsComponent implements OnInit {
+export class AnimalDetailsComponent implements OnInit, OnDestroy {
   animal: Animal;
+  private subscription?: Subscription;
 
   constructor(
     private animalService: AnimalService,
@@ -17,11 +19,15 @@ export class AnimalDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe((paramMap) => {
+    this.subscription = this.activatedRoute.paramMap.subscribe((paramMap) => {
       const id = paramMap.get('id');
       this.animalService.get(Number(id)).subscribe((data) => {
         this.animal = data;
       });
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 }
