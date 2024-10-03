85bb46888cd6ee471359dd26236b87096ca3f85e
Vincent Roth <vincent@placeme.io>
2024-10-03 16:37:49 +0200

    NGX Translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnimalModule } from './animal/animal.module';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  HttpClient,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 import { timeInterceptor } from './shared/time.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function httpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, './i18n/', '.json');
+  // ./i18n/en.json
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +33,14 @@ import { MatButtonModule } from '@angular/material/button';
     AnimalModule,
     MatToolbarModule,
     MatButtonModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'fr',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [
     provideHttpClient(withInterceptors([timeInterceptor])),
@@ -25,4 +48,8 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(translateService: TranslateService) {
+    translateService.getBrowserLang();
+  }
+}
